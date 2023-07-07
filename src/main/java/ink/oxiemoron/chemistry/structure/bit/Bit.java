458c3dfc8bd62daec2bb0354640c62b54e9be535
KayserSoze42 <morus.hana@gmail.com>
2023-07-07 22:17:40 +0200

    ink.oxiemoron.Ooff..
    changed a bit
    broke a byte
    will test

---
+++ src/main/java/ink/oxiemoron/chemistry/structure/bit/Bit.java
@@ -0,0 +1,17 @@
+package ink.oxiemoron.chemistry.structure.bit;
+
+public enum Bit {
+
+    Carbon("Carbon", +4),
+    Hydrogen("Hydrogen", +1),
+    Oxygen("Oxygen", -2);
+
+
+    public final int valency;
+    public final String name;
+
+    private Bit(String name, int valency) { // hahahahaha
+        this.name = name;
+        this.valency = valency;
+    }
+}
