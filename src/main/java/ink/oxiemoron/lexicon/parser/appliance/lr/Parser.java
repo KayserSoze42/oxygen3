85a6f1d2391c0eb9067987bc1cb8d2487963862a
KayserSoze42 <morus.hana@gmail.com>
2023-09-08 15:54:58 +0200

    ink.oxiemoron.tabula.rasa
    not the ai tho
    
    lol

--- src/main/java/ink/oxiemoron/lexicon/parser/appliance/lr/Parser.java
+++ src/main/java/ink/oxiemoron/lexicon/parser/appliance/lr/Parser.java
@@ -60,256 +60,43 @@ public class Parser implements ParserApproach<AST> {
 
     public AST rForm() throws IUPACSyntaxError {
         AST tree = new FormTree();
-        AST desDeclTree = rDesDeclTree();
-
-        keepdPile = new Pile<>();
-
-        if (lookAheadForCompound() || isNextToken(Tokens.String)) {
-
-            do {
-                keepdPile.push(currentToken);
-                scan();
-            } while (lookAheadForCompound() || isNextToken(Tokens.String));
-
-        }
-
-        while (!eos) {
-
-            if (isNextToken(Tokens.Allotr)) {
-
-                rDesDeclTree().addKid(rAllocrTree());
-
-            }
-
-            if (isNextToken(Tokens.Greatr) || isNextToken(Tokens.Lessr)) {
-
-                rDesDeclTree().addKid(rComparrTree());
-
-            }
-
-            if (isNextToken(Tokens.Askr)) {
-
-                tree.addKid(rAskrTree());
-            }
-
-        }
 
         return tree;
     }
 
-    public AST rDesDeclTree() throws IUPACSyntaxError {
-        AST tree = new DesDeclTree();
-
-        return tree;
-    }
-
-    public AST rSinDeclTree() throws IUPACSyntaxError {
-        AST tree = new SinDeclTree();
-
-        return tree;
-    }
-
-    public AST rPredicaTree() throws IUPACSyntaxError {
-        AST tree = new PredicaTree();
-
-        return tree;
-    }
-
-    public AST rAllocrTree() throws IUPACSyntaxError {
-
-        AST tree = new AllocrTree();
-
-        while (keepTolkien != null) {
-            if (isNextToken(Tokens.Location) || isNextToken(Tokens.Multiplier) || isNextToken(Tokens.Radical) || isNextToken(Tokens.Root)) {
-                tree.addKid(rSinTree().addKid(rCompoundTree()));
-            } else if (isNextToken(Tokens.String)) {
-                tree.addKid(rSinTree().addKid(rId()));
-            }
-        }
-
-
-        tree.addKid(rSteerr());
-
-        if (isNextToken(Tokens.Location) || isNextToken(Tokens.Multiplier) || isNextToken(Tokens.Radical) || isNextToken(Tokens.Root)) {
-            tree.addKid(rDesTree().addKid(rCompoundTree()));
-        } else if (isNextToken(Tokens.String)) {
-            tree.addKid(rDesTree().addKid(rString()));
-        }
-
-        return tree;
-    }
-
-    public AST rComparrTree() throws IUPACSyntaxError {
-
-        AST tree = new ComparrTree();
-
-
-        tree.addKid(rSteerr());
-
-        if (isNextToken(Tokens.Location) || isNextToken(Tokens.Multiplier) || isNextToken(Tokens.Radical) || isNextToken(Tokens.Root)) {
-            tree.addKid(rDesTree().addKid(rCompoundTree()));
-        }
-
-        if (isNextToken(Tokens.String)) {
-            tree.addKid(rDesTree().addKid(rId()));
-        }
-
-        return tree;
-    }
-
-    public AST rAskrTree() throws IUPACSyntaxError {
-        AST tree = new AskrTree();
-
-
-        expect(Tokens.Askr);
-
-        return tree;
-    }
-
-    public AST rDesTree() throws IUPACSyntaxError {
-        AST tree = new DesSideTree();
-
-        return tree;
-    }
-
-    public AST rSinTree() throws IUPACSyntaxError {
-        AST tree = new SinSideTree();
-
-        return tree;
-    }
-
-    public AST rSteerr() throws IUPACSyntaxError {
-
-        AST tree = new SteerrTree(currentToken);
-        scan();
-        return tree;
-
-    }
-
-    public AST rCompoundTree() throws IUPACSyntaxError {
-        AST tree = new CompoundTree();
-
-        while (isNextToken(Tokens.Location) || isNextToken(Tokens.Multiplier) || isNextToken(Tokens.Radical)) {
-
-            tree.addKid(rStructureTree());
-
-        }
-
-        if (isNextToken(Tokens.Root)) {
-
-            tree.addKid(rRoot());
-
-        } else {
-
-            if (eos) {
-                throw new IUPACSyntaxError(keepdPile.pop(), Tokens.Root);
-            }
-        }
-
-        scan();
-
-
-
-        if (isNextToken(Tokens.Semicolon)) {
-
-            expect(Tokens.Semicolon);
-
-        }
-
-        return tree;
-
-
-    }
-
-    public AST rStructureTree() throws IUPACSyntaxError {
-
-        AST tree = new StructureTree();
-
-        // Nothing for noe, we shall see shell
-
-        tree.addKid(rSubstituentTree());
-
-        return tree;
-    }
-
-    public AST rSubstituentTree() throws IUPACSyntaxError {
-
-        boolean radicalNeed = false;
-        boolean multiNeed = false;
-
-        AST tree = new SubstituentTree();
-
-        if (isNextToken(Tokens.Location)) {
-
-            if (!radicalNeed) {radicalNeed = true;}
-            if (!multiNeed) {multiNeed = true;}
-
-            do {
-
-                tree.addKid(rLocationTree());
-
-                if (isNextToken(Tokens.Comma)) {
-
-                    expect(Tokens.Comma);
-
-                }
-
-                if (isNextToken(Tokens.Dash)) {
-
-                    expect(Tokens.Dash);
-
-                }
-
-            } while (isNextToken(Tokens.Location));
-        }
-
-        if (isNextToken(Tokens.Multiplier)) {
-
-            if (!radicalNeed) {radicalNeed = true;}
-
-
-            tree.addKid(rMultiplierTree());
-
-            if (multiNeed) {multiNeed = false;}
-
-        } else {
-
-            if (multiNeed) {throw new IUPACSyntaxError(currentToken, Tokens.Multiplier);}
-
-        }
-
-        if (isNextToken(Tokens.Radical)) {
-
-            tree.addKid(rRadical());
-
-            if (radicalNeed) {radicalNeed = false;}
-
-        } else {
-            if (radicalNeed) {throw new IUPACSyntaxError(currentToken, Tokens.Radical);}
-        }
-
-        return tree;
-
-    }
-
-    public AST rLocationTree() throws IUPACSyntaxError {
-
+    public AST rLocation() throws IUPACSyntaxError {
         AST tree = new LocationTree(currentToken);
         scan();
         return tree;
     }
 
-    public AST rMultiplierTree() throws IUPACSyntaxError{
+    public AST rLocationPop() throws IUPACSyntaxError {
+        AST tree = new LocationTree(keepTolkien);
+        keepTolkien = keepdPile.pop();
+        return tree;
+    }
 
-        AST tree = new MultiplierTree(keepdPile.pop());
+    public AST rMultiplier() throws IUPACSyntaxError {
+        AST tree = new MultiplierTree(currentToken);
         scan();
         return tree;
     }
 
-    public AST rRadical() throws IUPACSyntaxError {
+    public AST rMultiplierPop() throws IUPACSyntaxError {
+        AST tree = new MultiplierTree(keepTolkien);
+        keepTolkien = keepdPile.pop();
+        return tree;
+    }
 
+    public AST rRadical() throws IUPACSyntaxError {
         AST tree = new RadicalTree(currentToken);
         scan();
+        return tree;
+    }
+
+    public AST rRadicalPop() throws IUPACSyntaxError {
+        AST tree = new RadicalTree(keepTolkien);
+        keepTolkien = keepdPile.pop();
         return tree;
     }
 
@@ -319,15 +106,33 @@ public class Parser implements ParserApproach<AST> {
         return tree;
     }
 
+    public AST rRootPop() throws IUPACSyntaxError {
+        AST tree = new RootTree(keepTolkien);
+        keepTolkien = keepdPile.pop();
+        return tree;
+    }
+
     public AST rString() throws IUPACSyntaxError {
         AST tree = new StringTree(currentToken);
         scan();
         return tree;
     }
 
+    public AST rStringPop() throws IUPACSyntaxError {
+        AST tree = new StringTree(keepTolkien);
+        keepTolkien = keepdPile.pop();
+        return tree;
+    }
+
     public AST rId() throws IUPACSyntaxError {
         AST tree = new IdTree(currentToken);
         scan();
+        return tree;
+    }
+
+    public AST rIdPop() throws IUPACSyntaxError {
+        AST tree = new IdTree(keepTolkien);
+        keepTolkien = keepdPile.pop();
         return tree;
     }
 
